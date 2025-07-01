a4f807a2f19c8d44f30d2ac1bfcea251e5deb4e4
amirhossein <amirhosseing983@gmail.com>
2025-07-02 01:26:40 +0330

    solution by golang

--- 001_2sum/solution.go
+++ 001_2sum/solution.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+
+
+
+func main() {
+	array := []int{1, 2, 3, 4}
+	res := twoSum(array, 6)
+	fmt.Println(res)
+}
+
+
+func twoSum(nums []int, target int) []int {
+	hash_map := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		diff := target - nums[i]
+		if val, ok := hash_map[diff]; ok {
+			return []int{val, i}
+		}else {
+			hash_map[nums[i]] = i
+		}
+	}
+	return nil
+    
+}
